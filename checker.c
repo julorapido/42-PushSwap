d7041bebf0c252f8a5ea693de2a7c2a6c33fcf4a
Jules Sainthorant <jsaintho@made-f0Dr10s16.clusters.42paris.fr>
2024-07-16 16:38:01 +0200

    W

--- checker.c
+++ checker.c
@@ -6,66 +6,42 @@
 /*   By: jsaintho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 12:10:19 by jsaintho          #+#    #+#             */
-/*   Updated: 2024/07/15 15:29:02 by jsaintho         ###   ########.fr       */
+/*   Updated: 2024/07/16 16:34:21 by jsaintho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	check_args(int *pos_i_arr, int *neg_i_arr, char **args)
+static long	check_args(char **args, long i)
 {
-	int	i;
-	int	j;
-	int	n;
+	long	j;
+	long	n;
 
-	i = 1;
 	while (args[i])
 	{
 		j = 0;
 		while (args[i][j] != '\0')
 		{
-			if (ft_isdigit((int)args[i][j]) == 0
-				&& args[i][j] != '-')
+			if (ft_isdigit((int)args[i][j]) == 0 && args[i][j] != '-')
 				return (-1);
 			j++;
 		}
-		n = ft_atoi(args[i]);
-		if ((pos_i_arr[n] && n >= 0) || (neg_i_arr[n] && n < 0))
+		n = ft_atoi_l(args[i]);
+		if (n <= -2147483648 || n > 2147483647)
 			return (-1);
-		if (n >= 0)
-			pos_i_arr[n] = 1;
-		else
-			neg_i_arr[n] = 1;
+		j = i + 1;
+		while (args[j])
+		{
+			if (ft_atoi_l(args[j]) == n)
+				return (-1);
+			j++;
+		}
 		i++;
 	}
 	return (1);
 }
 
-static int	init_i_arrs(char **args)
-{
-	int	*pos_arr;
-	int	*neg_arr;
-	int	i;
-
-	pos_arr = (int *) malloc(10000 * sizeof(int));
-	neg_arr = (int *) malloc(10000 * sizeof(int));
-	if (!pos_arr || !neg_arr)
-		return (-1);
-	i = 0;
-	while (i < 10000)
-	{
-		pos_arr[i] = 0;
-		neg_arr[i] = 0;
-		i++;
-	}
-	i = check_args(pos_arr, neg_arr, args);
-	free(pos_arr);
-	free(neg_arr);
-	return (i);
-}
-
 static int	init_stack(t_stack **s, int ac, char **av)
-
 {
 	int		i;
 	t_stack	*n;
@@ -73,18 +49,18 @@ static int	init_stack(t_stack **s, int ac, char **av)
 	t_stack	*first_;
 
 	i = 1;
+	n = 0;
 	ac++;
 	while (av[i])
 	{
 		prev = n;
-		n = ft_stacknew(ft_atoi(av[i]), prev);
+		n = ft_stacknew((long) ft_atoi(av[i]), prev);
 		if (i == 1)
 			first_ = n;
 		ft_stackadd_back(s, n);
 		i++;
 	}
 	first_->prev = n;
-	index_stack(s);
 	return (i - 1);
 }
 
@@ -154,7 +130,7 @@ static int	run_operation(char *op, t_stack **a, t_stack **b)
 static void	check(t_stack **a, t_stack **b)
 {
 	if (!is_sorted(a))
-		write(1 , "KO\n", 3);
+		write(1, "KO\n", 3);
 	if (is_sorted(a))
 		write(1, "OK\n", 3);
 	free_stack(a);
@@ -168,16 +144,14 @@ int	main(int argc, char **argv)
 	char		*line;
 
 	if (argc < 2)
-		return (-1);	
-	if (init_i_arrs(argv) == -1)
+		return (-1);
+	if (check_args(argv, 1) < 0)
 	{
-		write(1, "Error\n", 6);
+		write(2, "Error\n", 6);
 		return (-1);
 	}
 	a = (t_stack **) malloc(sizeof(t_stack **));
 	b = (t_stack **) malloc(sizeof(t_stack **));
-	if (!a || !b)
-		return (0);
 	*a = NULL;
 	*b = NULL;
 	init_stack(a, argc, argv);
